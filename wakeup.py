a49d2b8ce4fa7747159a14e70de3626818719dc2
Bartlomiej Bania <bartlomiej.bania@gmail.com>
2013-10-21 15:15:30 +0100

    more code cleaning

--- wakeup.py
+++ wakeup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-#These are the imports google said to include
 import gdata.calendar.service
 import gdata.service
-import atom.service
 import gdata.calendar
 import atom
+import atom.service
 import getopt
 import sys
 import string
@@ -18,16 +17,29 @@ import os, random #to play the mp3 later
 import logging
 
 logging.basicConfig()
- 
-# Login credentials
+
+#************************************************************************************# 
+#****           Login credentials for your Google Account                        ****#
+#************************************************************************************# 
+
 calendar_service = gdata.calendar.service.CalendarService()
-calendar_service.email = 'you@gmail.com' #your email
-calendar_service.password = '' #your password
+calendar_service.email = 'you@gmail.com'
+calendar_service.password = '***'
 calendar_service.source = 'SimpleGoogleAlarmClock'
 calendar_service.ProgrammaticLogin()
  
+#************************************************************************************# 
+#****           Global variables that can be changed                             ****#
+#************************************************************************************# 
+
+q = 'wake' #calendar query
+mp3_dir = "/home/pi/mp3/" #mp3 directory
+
+#************************************************************************************# 
+#****           Main querry definition                                           ****#
+#************************************************************************************# 
+
 def FullTextQuery(calendar_service):
-    q = 'wake'
     date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.000Z")
     endDate = (datetime.now() + timedelta(days=14)).strftime("%Y-%m-%dT%H:%M:%S.000Z")
     print 'Full text query for events on Primary Calendar: \'%s\'' % (q)
@@ -37,25 +49,23 @@ def FullTextQuery(calendar_service):
 #    query.ctz = 'Europe/London'
     query.singleevents = 'true'
     query.orderBy = 'startTime'
+    query.sortorder = 'a'
     query.max_results = '10'
     feed = calendar_service.CalendarQuery(query)
     for i, an_event in enumerate(feed.entry):
         for a_when in an_event.when:
             print " "
             print an_event.title.text ,"Scheduled:",i,"For:",time.strftime('%d-%m-%Y %H:%M',time.localtime(tf_from_timestamp(a_when.start_time))),"Current Time:",time.strftime('%d-%m-%Y %H:%M')
- 
             if time.strftime('%d-%m-%Y %H:%M',time.localtime(tf_from_timestamp(a_when.start_time))) == time.strftime('%d-%m-%Y %H:%M'):
                 print "Waking you up!"
-                print "---"
- 
-                songfile = random.choice(os.listdir("/home/pi/mp3/")) #chooses the .mp3 file
+                print "---" 
+                songfile = random.choice(os.listdir(mp3_dir)) #chooses the .mp3 file
                 print "Now Playing:", songfile
-                command ="mpg321" + " " + "/home/pi/mp3/" + "'"+songfile+"'"+ " -g 100" #plays the MP3 in it's entierty. As long as the song is longer than a minute then will only trigger once in the minute that start of the "wake" event
- 
+                command ="mpg321" + " " + mp3_dir + "'"+songfile+"'"+ " -g 100" #plays the MP3 in it's entierty. As long as the song is longer than a minute then will only trigger once in the minute that start of the event
                 print command
                 os.system(command) #runs the bash command
             else:
-                print "Wait for it..." #the "wake" event's start time != the system's current time
+                print "Wait for it..." #the event's start time is not the system's current time
  
 def callable_func():
     os.system("clear") #this is more for my benefit and is in no way necesarry
@@ -64,5 +74,5 @@ def callable_func():
     print "----------------------------"
  
 scheduler = Scheduler(standalone=True)
-scheduler.add_interval_job(callable_func,seconds=10)
-scheduler.start() #runs the program indefinatly on an interval of 5 seconds
+scheduler.add_interval_job(callable_func,seconds=5)
+scheduler.start() #runs the program indefinatly on an interval of 10 seconds
